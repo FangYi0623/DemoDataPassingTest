04b8e33991517d3fe5558c17bc2b8f9cc45f9655
FangYi0623 <20019648@myrp.edu.sg>
2021-05-24 11:10:00 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20019648/demodatapassingtest/MainActivity.java
@@ -0,0 +1,31 @@
+package sg.edu.rp.c346.id20019648.demodatapassingtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnPassInt, btnPassChar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnPassInt = findViewById(R.id.btnPassInt);
+
+        //Event handling Step 3
+        btnPassInt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("value", 1);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
